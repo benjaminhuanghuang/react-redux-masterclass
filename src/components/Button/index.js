46460247cdb8469c2cfec24c63e9d22eaa45cc2e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-12-03 17:40:24 -0800

    refact button and digits

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import ee from '../../eventEmitter';
 
+const Button = ({ text, buttonClass, clickHandler }) => (
+    <button className={buttonClass} 
+    onClick={()=>clickHandler ? clickHandler.call(null, text):null}>
+    <span className="title">{text}</span>
+    </button>
+);
+
+/*
 class Button extends Component {
     constructor(props) {
         super(props);
@@ -15,10 +23,10 @@ class Button extends Component {
 
     render() {
         return (
-            <button className={this.props.buttonClass} onClick={this.onClickHandler}>
+             <button className={this.props.buttonClass} onClick={this.onClickHandler}>
                 <span className="title">{this.props.text}</span>
             </button>
         );
     }
-}
+}*/
 export default Button;
