c8690f46d97e7a413494be9dc953436086ff7335
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-12-03 22:30:02 -0800

    refact digits

--- src/components/Digits/index.js
+++ src/components/Digits/index.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import ee from '../../eventEmitter';
 //
 import Button from '../Button';
-import store from  '../../store';
+import store from '../../store';
 
-Digits = () =>(
+const digitClickHandler = (num) => {
+    if (!store.curExpression) {
+        return store.newExpression = num;
+    }
+
+    return store.newExpression = `${store.curExpression}${num}`;
+}
+
+
+const Digits = () => (
     <section className="buttons--digits">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-        .map(nr => <Button key={nr} text={nr} clickHandler={this.digitClickHandler}/>)}
+            .map(nr => <Button key={nr} text={nr} clickHandler={digitClickHandler} />)}
     </section>
 )
 // class Digits extends Component {
